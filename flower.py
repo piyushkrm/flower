3a9bfc32387a10a2157b281223990abc68a9d6ca
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-08-12 11:04:44 +0530

    complete

--- flower.py
+++ flower.py
@@ -12,3 +12,11 @@ for j in range(25) :
     for i in range(25) :
         tur.color(cs.hsv_to_rgb(i/15, j/25, 1))
         tur.right(90)
+        tur.circle(200-j*4, 90)
+        tur.left(90)
+        tur.circle(200-j*4, 90)
+        tur.right(180)
+        tur.circle(50, 24)
+
+tur.hideturtle()
+tur.done()
